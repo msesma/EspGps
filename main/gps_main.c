f22e9291c42e3c7a2c6d99fb988f03765b002ef6
Miguel Sesma <miguel.sesma@medtronic.com>
2025-03-29 17:27:05 +0100

    Show speed on display in km/h

--- main/gps_main.c
+++ main/gps_main.c
@@ -28,19 +28,13 @@ static const char *TAG = "EspGps";
 static uint8_t s_led_state = 0;
 static uint16_t s_led_period = 1000;
 
-static uint16_t s_wait_time = 1000 / portTICK_PERIOD_MS;
+static uint16_t s_wait_time = 500 / portTICK_PERIOD_MS;
 const gpio_num_t LED_CLK = CONFIG_TM1637_CLK_PIN;
 const gpio_num_t LED_DTA = CONFIG_TM1637_DIO_PIN;
 
-// const uart_port_t uart_num = UART_NUM_0;
-// uart_config_t uart_config = {
-//     .baud_rate = 9600,
-//     .data_bits = UART_DATA_8_BITS,
-//     .parity = UART_PARITY_DISABLE,
-//     .stop_bits = UART_STOP_BITS_1,
-//     .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-//     .rx_flow_ctrl_thresh = 122,
-// };
+uint16_t speed_kmh = 0;
+uint8_t fix_state = GPS_MODE_INVALID;  // GPS_MODE_INVALID, GPS_MODE_2D, GPS_MODE_3D
+static const int s_led_brightness = 7; // 0-7
 
 /**
  * @brief GPS Event Handler
@@ -66,6 +60,13 @@ static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_ba
                  gps->date.year + YEAR_BASE, gps->date.month, gps->date.day,
                  gps->tim.hour + TIME_ZONE, gps->tim.minute, gps->tim.second,
                  gps->latitude, gps->longitude, gps->altitude, gps->speed);
+
+        // taskENTER_CRITICAL();
+        uint16_t speed_kmh_calc = gps->speed * 3.6; // convert m/s to km/h
+        speed_kmh = speed_kmh_calc;
+        fix_state = gps->fix_mode;
+        // taskEXIT_CRITICAL();
+
         break;
     case GPS_UNKNOWN:
         /* print unknown statements */
@@ -82,124 +83,31 @@ void tm1637_task(void *arg)
     if (led == NULL)
         vTaskDelete(NULL);
 
-#if 0
-        tm1637_set_brightness(led, 7);
-        while (true)
-        {
-            tm1637_set_segment_fixed(led, led->segment_idx[0], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[1], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[2], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[3], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[4], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[5], 0xFF);
-            vTaskDelay(s_wait_time);
-        }
-#endif
+    tm1637_set_brightness(led, s_led_brightness);
 
     while (true)
     {
-        // Test segment control
-        ESP_LOGI(TAG, "Test segment control");
-        uint8_t seg_data[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20};
-        for (uint8_t x = 0; x < 32; ++x)
-        {
-            uint8_t v_seg_data = seg_data[x % 6];
-            tm1637_set_segment_fixed(led, led->segment_idx[0], v_seg_data);
-            tm1637_set_segment_fixed(led, led->segment_idx[1], v_seg_data);
-            tm1637_set_segment_fixed(led, led->segment_idx[2], v_seg_data);
-            tm1637_set_segment_fixed(led, led->segment_idx[3], v_seg_data);
-            tm1637_set_segment_fixed(led, led->segment_idx[4], v_seg_data);
-            tm1637_set_segment_fixed(led, led->segment_idx[5], v_seg_data);
-            vTaskDelay(s_wait_time);
-        }
-
-        // Test brightness
-        ESP_LOGI(TAG, "Test brightness");
-        for (int x = 0; x < 1; x++)
-        {
-            tm1637_set_brightness(led, x);
-            tm1637_set_segment_fixed(led, led->segment_idx[0], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[1], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[2], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[3], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[4], 0xFF);
-            tm1637_set_segment_fixed(led, led->segment_idx[5], 0xFF);
-            vTaskDelay(s_wait_time);
-        }
+        ESP_LOGI(TAG, "Display speed");
+        tm1637_set_number(led, speed_kmh, false, 0x00);
         vTaskDelay(s_wait_time);
-
-        // Test display integer number
-        ESP_LOGI(TAG, "Test display integer number");
-        tm1637_set_number(led, 1, true, 0x00); // 0001
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, 12, true, 0x00); // 0012
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, 123, true, 0x00); // 0123
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, 1234, true, 0x00); // 1234
-        vTaskDelay(s_wait_time);
-
-        tm1637_set_number(led, 1, false, 0x00); // ____1
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, 12, false, 0x00); // ____12
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, 123, false, 0x00); // ___123
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, 1234, false, 0x00); // __1234
-        vTaskDelay(s_wait_time);
-
-        tm1637_set_number(led, -1, true, 0x00); // -001
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, -12, true, 0x00); // -012
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, -123, true, 0x00); // -123
-        vTaskDelay(s_wait_time);
-
-        tm1637_set_number(led, -1, false, 0x00); // ____-1
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, -12, false, 0x00); // ___-12
-        vTaskDelay(s_wait_time);
-        tm1637_set_number(led, -123, false, 0x00); // __-123
-        vTaskDelay(s_wait_time);
-
-        // Test display text
-        ESP_LOGI(TAG, "Test display text");
-        tm1637_set_segment_ascii(led, "PLAY");
-        vTaskDelay(s_wait_time);
-        tm1637_set_segment_ascii(led, "1234567890");
-        vTaskDelay(s_wait_time);
-        tm1637_set_segment_ascii(led, "IP 192.168.10.20");
-        vTaskDelay(s_wait_time);
-        tm1637_set_segment_ascii(led, "STOP");
-        vTaskDelay(s_wait_time);
-
-        // Test clock segment
-        ESP_LOGI(TAG, "Test clock segment 1");
-        tm1637_set_segment_ascii_with_time(led, "1234", 0x40, 1000);
-        vTaskDelay(s_wait_time);
-        ESP_LOGI(TAG, "Test clock segment 2");
-        tm1637_set_segment_ascii_with_time(led, "1234", 0x00, 1000);
-        vTaskDelay(s_wait_time);
-    } // end while
-}
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
+    }
 }
 
 void led_task(void *arg)
 {
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while (1)
+    while (true)
     {
-        // ESP_LOGI(TAG, "s_led_period %d", s_led_period);
-        // ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(s_led_period / portTICK_PERIOD_MS);
+        if (fix_state == GPS_MODE_INVALID)
+        {
+            gpio_set_level(BLINK_GPIO, s_led_state);
+            s_led_state = !s_led_state;
+            vTaskDelay(s_led_period / portTICK_PERIOD_MS);
+        }
+        else
+        {
+            gpio_set_level(BLINK_GPIO, 1);
+        }
     }
 }
 
@@ -209,6 +117,7 @@ void app_main(void)
     ESP_LOGI(TAG, "GPS LOG");
 
     xTaskCreate(&tm1637_task, "tm1637_task", 1024 * 4, NULL, 5, NULL);
+
     xTaskCreate(&led_task, "led_task", 1024 * 2, NULL, 5, NULL);
 
     /* NMEA parser configuration */
@@ -217,11 +126,4 @@ void app_main(void)
     nmea_parser_handle_t nmea_hdl = nmea_parser_init(&config);
     /* register event handler for NMEA parser library */
     nmea_parser_add_handler(nmea_hdl, gps_event_handler, NULL);
-
-    // vTaskDelay(10000 / portTICK_PERIOD_MS);
-
-    /* unregister event handler */
-    // nmea_parser_remove_handler(nmea_hdl, gps_event_handler);
-    /* deinit NMEA parser library */
-    // nmea_parser_deinit(nmea_hdl);
 }
